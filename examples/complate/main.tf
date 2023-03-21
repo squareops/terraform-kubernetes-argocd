237c26cd6ca4d2930cc984cb002afa3370379ffe
RohitSquareops <rohit.kumar@squareops.com>
2023-03-21 10:27:04 +0000

    Update main.tf

--- examples/complate/main.tf
+++ examples/complate/main.tf
@@ -9,11 +9,11 @@ locals {
 module "argocd" {
   source = "../../"
   argocd_config = {
-    hostname                  = "argoc.ref.stg.skaf.squareops.in"
+    hostname                  = "argoc.squareops.in"
     autoscaling_enabled       = true
     redis_ha_enable           = true
     enable_argo_notifications = true
-    slack_token               = "xoxb-379541400966-4559734786594-qQ8486bluEuvmxrYxRatsM8R"
+    slack_token               = "xoxb-qQ8486bluEuvmxrYx"
     values_yaml               = file("./helm/values.yaml")
   }
 }
