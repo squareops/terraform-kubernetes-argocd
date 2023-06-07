250135eda3e2d9356c01addf7a3c7f9064688592
Rohit Singh <107384244+RohitSquareops@users.noreply.github.com>
2023-06-07 12:08:07 +0530

    Update repo reference in main.tf

--- examples/complate/main.tf
+++ examples/complate/main.tf
@@ -9,9 +9,9 @@ locals {
 }
 
 module "argocd" {
-  source = "squareops/argocd/kubernetes.git"
+  source = "squareops/argocd/kubernetes"
   argocd_config = {
-    hostname                     = "argocd.org.in"
+    hostname                     = "argocd.prod.in"
     values_yaml                  = file("./helm/values.yaml")
     redis_ha_enabled             = true
     autoscaling_enabled          = true
