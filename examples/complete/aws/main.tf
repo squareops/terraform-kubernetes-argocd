06ceb4e39319a0cd2825a08b6137eb7870b4e2e2
prajwalakhuj <prajwalakhuj21@gmail.com>
2023-08-25 17:20:58 +0530

    removed slack token

--- examples/complete/aws/main.tf
+++ examples/complete/aws/main.tf
@@ -15,7 +15,7 @@ module "argocd" {
     values_yaml                  = file("./helm/values.yaml")
     redis_ha_enabled             = true
     autoscaling_enabled          = true
-    slack_notification_token     = "xoxb-qQ8486bluEuvmxrYx"
+    slack_notification_token     = ""
     argocd_notifications_enabled = true
   }
 }
