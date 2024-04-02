e0da49d6398f0ff609cdd20008c211636db928e8
rachit89 <rachit.maheshwari@squareops.com>
2024-04-02 15:23:41 +0530

    updated

--- variable.tf
+++ variable.tf
@@ -11,7 +11,7 @@ variable "argocd_config" {
   description = "Specify the configuration settings for Argocd, including the hostname, redis_ha_enabled, autoscaling, notification settings, and custom YAML values."
 }
 
-variable "chart_version" {
+variable "helm_chart_version" {
   type        = string
   default     = "6.7.3"
   description = "Version of the Argocd chart that will be used to deploy Argocd application."
