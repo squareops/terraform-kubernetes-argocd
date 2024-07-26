41a474fe1e796490623958f94ec89589c0549774
vinayakops <vinayak.gautam@revnue.com>
2024-07-26 13:31:07 +0530

    Fixed argocd ingress certificate and 502 issuse.

--- variable.tf
+++ variable.tf
@@ -7,13 +7,14 @@ variable "argocd_config" {
     autoscaling_enabled          = false
     slack_notification_token     = ""
     argocd_notifications_enabled = false
+    ingress_class_name           = ""
   }
   description = "Specify the configuration settings for Argocd, including the hostname, redis_ha_enabled, autoscaling, notification settings, and custom YAML values."
 }
 
 variable "chart_version" {
   type        = string
-  default     = "5.4.0"
+  default     = "7.3.11"
   description = "Version of the Argocd chart that will be used to deploy Argocd application."
 }
 
