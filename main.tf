b3f62a2e6439be9f4a865ff8fd9555e6baf23d4d
vinayakgautamops <95210787+vinayakgautamops@users.noreply.github.com>
2024-08-20 13:50:38 +0530

    Release 3.0.1 (#18)
    * ArgoCD Version Upgrade 7.3.10 (#16)
    
    * Version upgrade ArgoCD
    
    * Ingress changes for argocd-server
    
    ---------
    
    * Fixed argocd ingress certificate and 502 issuse.

--- main.tf
+++ main.tf
@@ -19,6 +19,8 @@ resource "helm_release" "argocd_deploy" {
       redis_ha_enable           = var.argocd_config.redis_ha_enabled
       autoscaling_enabled       = var.argocd_config.autoscaling_enabled
       enable_argo_notifications = var.argocd_config.argocd_notifications_enabled
+      ingress_class_name        = var.argocd_config.ingress_class_name
+
     }),
     var.argocd_config.values_yaml
   ]
--- variable.tf
+++ variable.tf
@@ -7,13 +7,14 @@ variable "argocd_config" {
     autoscaling_enabled          = false
     slack_notification_token     = ""
     argocd_notifications_enabled = false
+    ingress_class_name           = ""
   }
   description = "Specify the configuration settings for Argocd, including the hostname, redis_ha_enabled, autoscaling, notification settings, and custom YAML values."
 }
 
 variable "chart_version" {
   type        = string
-  default     = "5.53.11"
+  default     = "7.3.11"
   description = "Version of the Argocd chart that will be used to deploy Argocd application."
 }
 
@@ -21,4 +22,10 @@ variable "namespace" {
   type        = string
   default     = "argocd"
   description = "Name of the Kubernetes namespace where the Argocd deployment will be deployed."
+}
+
+variable "ingress_class_name" {
+  type = string
+  default = "ingress-nginx"
+  description = "Enter ingress class name which is created in EKS cluster"
 }
